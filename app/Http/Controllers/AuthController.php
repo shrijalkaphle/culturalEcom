4d5c4c41413b0e85d5f31130e1af990cfd3c85c1
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2022-05-01 14:00:52 +0545

    email verified

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterValidationRequest;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -18,6 +19,7 @@ class AuthController extends Controller
             
         $input['role_id'] = 2;
         $input['password'] = Hash::make($input['password']);
+        $input['email_verified_at'] = Carbon::now();
         User::create($input);
 
         return redirect()->route('page.login')->with('success', 'Verify Emaill to continue');
